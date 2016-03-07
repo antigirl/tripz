8508721873b2ba34e5a27dcef64d3bdebf4bb276
Farzad Qasim <qasimf@tcd.ie>
2016-03-07 15:46:58 +0000

    Update to search taxonomy

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -9,29 +9,53 @@ export default class Search extends Component {
         super(props);
 
         this.state = {
-            tags: []
+            tags: [],
+            activeCategory: 'occupancy'
         };
     }
 
-    handlePress(e) {
-        var textValue = ReactDOM.findDOMNode(this.refs.editor).value;
-        ReactDOM.findDOMNode(this.refs.editor).placeholder = '';
-        if (e.keyCode === 9) {
-            this.setState({
-                tags: this.state.tags.concat([textValue])
-            });
-            ReactDOM.findDOMNode(this.refs.editor).value = '';
-            this.props.actions.getEvents();
+    componentDidMount() {
+        this.inputDOMnode = ReactDOM.findDOMNode(this.refs.input);
 
-        }
+        this.setState({
+            tags: [{
+                type: 'location',
+                val: 'Norwich'
+            }]
+        });
     }
 
-    addItem(item) {
-        ReactDOM.findDOMNode(this.refs.editor).placeholder = '';
-        this.setState({
-            tags: this.state.tags.concat([item])
-        });
-        this.props.actions.getEvents();
+    handlePress(e) {
+        e.preventDefault();
+        // var textValue = this.inputDOMnode.value;
+        // this.inputDOMnode.placeholder = '';
+        // if (e.keyCode === 9) {
+        //     this.setState({
+        //         tags: this.state.tags.concat([{
+        //             type: 'activity',
+        //             val: textValue
+        //         }])
+        //     });
+        //     this.inputDOMnode.value = '';
+        //     this.inputDOMnode.focus();
+        //     this.props.actions.getEvents();
+        //
+        // }
+    }
+
+    addItem(type, e) {
+        //this.inputDOMnode.placeholder = '';
+        const itemsOnly = e.target.className !== 'search__options';
+        const item = ReactDOM.findDOMNode(e.target).innerHTML.toLowerCase();
+        if (itemsOnly) {
+            this.setState({
+                tags: this.state.tags.concat([{
+                    type: type,
+                    val: item
+                }])
+            });
+            this.props.actions.getEvents();
+        }
     }
 
     removeItem(tag) {
@@ -52,6 +76,16 @@ export default class Search extends Component {
         }
     }
 
+    setCategory(e) {
+        const category = ReactDOM.findDOMNode(e.target).innerHTML.toLowerCase();
+        const itemsOnly = e.target.className !== 'search__categories';
+        if (itemsOnly) {
+            this.setState({
+                activeCategory: category
+            });
+        }
+    }
+
     render() {
         return(
             <header>
@@ -59,30 +93,53 @@ export default class Search extends Component {
                     <form>
                         <div className="search__inputwrapper">
                             {this.state.tags.map((tag, i)=> {
-                                let tagType = 'tag__location';
-                                if (i === 1) {
-                                    tagType = 'tag__date';
-                                }
-                                if (i == 2) {
-                                    tagType = 'tag__holidaytype';
-                                }
-                                if (i >= 3) {
-                                    tagType = 'tag__activity';
-                                }
-                                const tagTypeClass = classNames('tag', tagType);
-                                return <div className={tagTypeClass} key={i}>{tag}<span className="tag__cancel" onClick={() => this.removeItem(tag)}>x</span></div>
+                                const tagTypeClass = classNames('tag', 'tag__' + tag.type);
+                                return <div className={tagTypeClass} key={i}>{tag.val}<span className="tag__cancel" onClick={() => this.removeItem(tag)}>x</span></div>
                             })}
 
-                            <input type="text" className="search__input" ref="editor" onKeyDown={this.handlePress.bind(this)} placeholder="Enter or Select location, date, holiday type and activities"/>
+                            <input type="text" className="search__input" ref="input" onKeyDown={this.handlePress.bind(this)} placeholder="Enter or Select location, date, holiday type and activities"/>
                         </div>
-
                         <button className="search__button" onClick={(e) => this.handleSubmit(e)}>SEARCH</button>
                     </form>
 
-                    {this.state.tags.length === 0 ? <ul className="search__options"><li onClick={()=>this.addItem('Manchester')}>Manchester</li><li onClick={()=>this.addItem('London')}>London</li><li onClick={()=>this.addItem('Glasgow')}>Glasgow</li><li onClick={()=>this.addItem('Birmingham')}>Birmingham</li></ul> : null}
-                    {this.state.tags.length === 1 ? <ul className="search__options"><li onClick={()=>this.addItem('5th March 2015')}>5th March 2015</li><li onClick={()=>this.addItem('12th March 2015')}>12th March 2015</li><li onClick={()=>this.addItem('19th March 2015')}>19th March 2015</li><li onClick={()=>this.addItem('26th March 2015')}>26th March 2015</li></ul>: null}
-                    {this.state.tags.length === 2 ? <ul className="search__options"><li onClick={()=>this.addItem('Family Holiday')}>Family Holiday</li><li onClick={()=>this.addItem('Romantic Getaway')}>Romantic Getaway</li><li onClick={()=>this.addItem('Groups')}>Groups</li><li onClick={()=>this.addItem('Couples')}>Couples</li></ul>: null}
-                    {this.state.tags.length >= 3 ? <ul className="search__options"><li onClick={()=>this.addItem('Music')}>Music</li><li onClick={()=>this.addItem('Eating')}>Eating</li><li onClick={()=>this.addItem('Outdoors')}>Outdoors</li><li onClick={()=>this.addItem('Night Life')}>Night Life</li></ul>: null}
+
+                    {this.state.activeCategory === 'occupancy' ?
+                        <ul className="search__options" onClick={this.addItem.bind(this, 'occupancy')}>
+                            <li className="tag__occupancy">+1 Adult</li>
+                            <li className="tag__occupancy">+1 Child</li>
+                        </ul> : null }
+
+                        {this.state.activeCategory === 'dates' ?
+                            <ul className="search__options" onClick={this.addItem.bind(this, 'date')}>
+                                <li className="tag__date">Select Date</li>
+                                <li className="tag__date">Tonight</li>
+                                <li className="tag__date">Tomorrow</li>
+                                <li className="tag__date">This Weekend</li>
+                                <li className="tag__date">Next Weekend</li>
+                                <li className="tag__date">Next Month</li>
+                            </ul> : null }
+
+                            {this.state.activeCategory === 'activities' ?
+                                <ul className="search__options" onClick={this.addItem.bind(this, 'activity')}>
+                                    <li className="tag__activity">Music</li>
+                                    <li className="tag__activity">Eating</li>
+                                    <li className="tag__activity">Culture</li>
+                                    <li className="tag__activity">Outdoors</li>
+                                    <li className="tag__activity">Indoors</li>
+                                    <li className="tag__activity">Family</li>
+                                    <li className="tag__activity">Romantic</li>
+                                    <li className="tag__activity">Sight seeing</li>
+                                    <li className="tag__activity">Shopping</li>
+                                    <li className="tag__activity">Night Life</li>
+                                    <li className="tag__activity">Tours</li>
+                                </ul> : null }
+
+                    <ul className="search__categories" onClick={this.setCategory.bind(this)}>
+                        <li className={this.state.activeCategory === 'occupancy'? 'active' : null }>Occupancy</li>
+                        <li className={this.state.activeCategory === 'dates'? 'active' : null }>Dates</li>
+                        <li className={this.state.activeCategory === 'activities'? 'active' : null }>Activities</li>
+                    </ul>
+
                 </div>
             </header>
         )
