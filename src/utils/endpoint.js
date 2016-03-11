cd8fbfa0900bc404980215a904ca3374f999346b
Farzad Qasim <qasimf@tcd.ie>
2016-03-11 14:47:46 +0000

    Fix to work locally

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,4 +1,4 @@
-const host = window.location.host.indexOf('localhost') > -1 ? 'http://localhost:3000' : 'remoteURL';
+const host = window.location.host.indexOf('localhost') || window.location.host.indexOf('file://') > -1 ? 'http://localhost:3000' : 'remoteURL';
 
 export function getEndPoint() {
     return host;
