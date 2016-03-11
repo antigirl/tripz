0bf5dd553ea90922d7a628d366d6b9e1bd724472
Alex Perry <alexperry2@gmail.com>
2016-03-11 15:25:01 +0000

    Updated endpoint js

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,4 +1,4 @@
-const host = window.location.host.indexOf('localhost') || window.location.host.indexOf('file://') > -1 ? 'http://localhost:3000' : 'remoteURL';
+const host = (window.location.host.indexOf('localhost') > -1 || window.location.host.indexOf('file://') > -1) ? 'http://localhost:3000' : 'http://labs.laterooms.com/trips-api';
 
 export function getEndPoint() {
     return host;
